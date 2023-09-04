099853274f03f1e4027a72ef35b2769514fd39fc
PatchOfScotland <d.marchant@ed-alumni.net>
2023-09-04 10:55:09 +0200

    added two additional code files to c lecture code

---
+++ lectures/23-09-04_intro_and_c/lecture_code/side_one.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Note this is not something to copy
+int main() {
+  int a = 3;
+  int b = 5;
+  int c = a++ + ++b;
+
+  printf("a = %d\n", a);
+  printf("b = %d\n", b);
+  printf("c = %d\n", c);
+}
---
+++ lectures/23-09-04_intro_and_c/lecture_code/side_two.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Note this is not something to copy
+int main() {
+  int a = 3;
+  int b = 5;
+  int c = -2;
+  a += (c = a++ - (b += --c)) + ++b;
+  
+  printf("a = %d\n", a);
+  printf("b = %d\n", b);
+  printf("c = %d\n", c);
+}
